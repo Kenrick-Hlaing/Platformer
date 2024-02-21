4d3db295e3ed5c12e3ff74986649b971df97160d
Ken Hlaing <kenrick0102@gmail.com>
2024-02-21 13:39:34 -0800

    Initial Commit

---
+++ Assets/Platformer/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI coinText;
+    public int coinScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int intTime = 360 - (int)Time.realtimeSinceStartup;
+        if(intTime <= 0){
+            intTime = 0;
+        }
+        string timeStr = $"TIME\n{intTime}";
+        timerText.text = timeStr;
+
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Cast a ray from the mouse position into the scene
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Check if the ray hits something
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit Object Name: " + hit.collider.gameObject.name);
+                // If hit a brick
+                if(hit.collider.gameObject.name == "Brick(Clone)")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+                // If hit a '?'
+                if(hit.collider.gameObject.name == "Question(Clone)")
+                {
+                    coinScore++;
+                    coinText.text = $"<sprite=0>x{coinScore}";
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Platformer/Scripts/LevelParser.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LevelParser : MonoBehaviour
+{
+    public string filename;
+    public Transform environmentRoot;
+
+    [Header("Block Prefabs")]
+    public GameObject rockPrefab;
+    public GameObject brickPrefab;
+    public GameObject questionBoxPrefab;
+    public GameObject stonePrefab;
+    
+    // --------------------------------------------------------------------------
+    void Start()
+    {
+        LoadLevel();
+    }
+
+    // --------------------------------------------------------------------------
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadLevel();
+        }
+    }
+
+    // --------------------------------------------------------------------------
+    private void LoadLevel()
+    {
+        string fileToParse = $"{Application.dataPath}{"/Resources/"}{filename}.txt";
+        Debug.Log($"Loading level file: {fileToParse}");
+
+        Stack<string> levelRows = new Stack<string>();
+
+        // Get each line of text representing blocks in our level
+        using (StreamReader sr = new (fileToParse))
+        {
+            while (sr.ReadLine() is { } line)
+            {
+                levelRows.Push(line);
+            }
+
+            sr.Close();
+        }
+
+        // Use this variable in the todo code
+        int row = 0;
+
+        // Go through the rows from bottom to top
+        while (levelRows.Count > 0)
+        {
+            string currentLine = levelRows.Pop();
+
+            char[] letters = currentLine.ToCharArray();
+            for (var col = 0; col < letters.Length; col++)
+            {
+                var letter = letters[col];
+                // Todo - Instantiate a new GameObject that matches the type specified by letter
+                // Todo - Position the new GameObject at the appropriate location by using row and column
+                // Todo - Parent the new GameObject under levelRoot
+                Vector3 newPos = new Vector3(col, row, 0f);
+                if(letter == 'x'){
+                    
+                    Instantiate(rockPrefab, newPos, Quaternion.identity, environmentRoot);
+                } else if (letter == 'b'){
+                    Instantiate(brickPrefab, newPos, Quaternion.identity, environmentRoot);
+                } else if (letter == 's'){
+                    Instantiate(stonePrefab, newPos, Quaternion.identity, environmentRoot);
+                } else if (letter == '?'){
+                    Instantiate(questionBoxPrefab, newPos, Quaternion.identity, environmentRoot);
+                }
+                
+            }
+            row++;
+        }
+    }
+
+    // --------------------------------------------------------------------------
+    private void ReloadLevel()
+    {
+        foreach (Transform child in environmentRoot)
+        {
+           Destroy(child.gameObject);
+        }
+        LoadLevel();
+    }
+}
